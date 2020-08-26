ad21e6bf109ffe738a5cd758cdd70d375917e395
wangxiaohao <wangxiaohao13@gmail.com>
2020-08-26 23:37:00 +0800

    add onClickListenering2

--- app/src/main/java/com/kg/magisk/hello_world/MainActivity.java
+++ app/src/main/java/com/kg/magisk/hello_world/MainActivity.java
@@ -3,6 +3,8 @@ package com.kg.magisk.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("MainActivity","is login");
+                Log.d("MainActivity","is login");
+
+
+            }
+        });
     }
 }

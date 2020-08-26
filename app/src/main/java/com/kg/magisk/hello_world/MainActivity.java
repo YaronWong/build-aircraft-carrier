754b6b56e022ca4e8e9b0e48d15234faa757d2d0
wangxiaohao <wangxiaohao13@gmail.com>
2020-08-26 23:19:28 +0800

    add mian view

---
+++ app/src/main/java/com/kg/magisk/hello_world/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kg.magisk.hello_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
